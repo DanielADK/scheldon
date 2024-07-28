5a500b6a322f9966f5b3ae174e9bda248cb5d481
Daniel Adámek <daniel.adamek123@icloud.com>
2024-07-28 23:43:15 +0200

    swagger support

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import students from './students';
 import employees from './employee';
 import classes from './class';
 import rooms from './room';
+import swagger from './swagger';
 
 const router = new Router();
 
@@ -12,5 +13,7 @@ router.use(students.routes());
 router.use(employees.routes());
 router.use(classes.routes());
 router.use(rooms.routes());
+// Swagger
+router.use(swagger.routes());
 
 export default router;
