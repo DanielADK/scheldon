fa9ea6c270ad6d8f45cbd92d59db89e333e7f46b
Daniel Adámek <daniel.adamek123@icloud.com>
2024-07-28 23:07:28 +0200

    tslint update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,11 @@
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import eslintConfigPrettier from 'eslint-config-prettier';
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
 
 
 export default [
   {files: ["**/*.{js,mjs,cjs,ts}"]},
-  eslint.configs.recommended,
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  ...tseslint.configs.strict,
-  ...tseslint.configs.stylistic,
-  eslintConfigPrettier
 ];
