d0437caf44c40da340bf936772d27fd1150cd1e1
Daniel Adámek <daniel.adamek123@icloud.com>
2024-07-29 14:22:41 +0200

    employee, subject example data

---
+++ src/tests/data/employeeData.ts
@@ -0,0 +1,672 @@
+export const employee = [
+  {
+    username: 'jnovak',
+    name: 'Jan',
+    surname: 'Novák',
+    degreePre: 'Bc.',
+    degreePost: 'Ph.D.',
+    abbreviation: 'JN',
+    isTeacher: true,
+    isActive: true
+  },
+  {
+    username: 'hhajek',
+    name: 'Hana',
+    surname: 'Hájek',
+    degreePre: 'Bc.',
+    degreePost: 'Mgr.',
+    abbreviation: 'HH',
+    isTeacher: true,
+    isActive: true
+  },
+  {
+    username: 'mprochazka',
+    name: 'Martin',
+    surname: 'Procházka',
+    degreePre: null,
+    degreePost: null,
+    abbreviation: 'MP',
+    isTeacher: false,
+    isActive: false
+  },
+  {
+    username: 'alebeda',
+    name: 'Aleš',
+    surname: 'Lebeda',
+    degreePre: 'Ing.',
+    degreePost: 'Ph.D.',
+    abbreviation: 'AL',
+    isTeacher: true,
+    isActive: true
+  },
+  {
+    username: 'knespor',
+    name: 'Karolína',
+    surname: 'Něspoř',
+    degreePre: 'Mgr.',
+    degreePost: null,
+    abbreviation: 'KN',
+    isTeacher: false,
+    isActive: false
+  },
+  {
+    username: 'sjanda',
+    name: 'Simona',
+    surname: 'Janda',
+    degreePre: 'Ing.',
+    degreePost: null,
+    abbreviation: 'SJ',
+    isTeacher: false,
+    isActive: false
+  },
+  {
+    username: 'lkrejci',
+    name: 'Lukáš',
+    surname: 'Krejčí',
+    degreePre: 'Bc.',
+    degreePost: 'Ph.D.',
+    abbreviation: 'LK',
+    isTeacher: true,
+    isActive: true
+  },
+  {
+    username: 'psevcik',
+    name: 'Pavel',
+    surname: 'Ševčík',
+    degreePre: null,
+    degreePost: 'Ing.',
+    abbreviation: 'PS',
+    isTeacher: true,
+    isActive: false
+  },
+  {
+    username: 'aholub',
+    name: 'Alena',
+    surname: 'Holub',
+    degreePre: 'Bc.',
+    degreePost: null,
+    abbreviation: 'AH',
+    isTeacher: false,
+    isActive: true
+  },
+  {
+    username: 'jvavra',
+    name: 'Jakub',
+    surname: 'Vávra',
+    degreePre: 'Ing.',
+    degreePost: 'Ph.D.',
+    abbreviation: 'JV',
+    isTeacher: true,
+    isActive: false
+  },
+  {
+    username: 'fliska',
+    name: 'Filip',
+    surname: 'Liška',
+    degreePre: 'Bc.',
+    degreePost: null,
+    abbreviation: 'FL',
+    isTeacher: true,
+    isActive: false
+  },
+  {
+    username: 'dsramek',
+    name: 'David',
+    surname: 'Srámeček',
+    degreePre: 'Ing.',
+    degreePost: null,
+    abbreviation: 'DS',
+    isTeacher: false,
+    isActive: true
+  },
+  {
+    username: 'jmarek',
+    name: 'Jana',
+    surname: 'Marek',
+    degreePre: 'Mgr.',
+    degreePost: 'Ph.D.',
+    abbreviation: 'JM',
+    isTeacher: true,
+    isActive: false
+  },
+  {
+    username: 'ztichy',
+    name: 'Zdeněk',
+    surname: 'Tichý',
+    degreePre: 'Bc.',
+    degreePost: null,
+    abbreviation: 'ZT',
+    isTeacher: false,
+    isActive: true
+  },
+  {
+    username: 'kvybiral',
+    name: 'Karel',
+    surname: 'Výbíral',
+    degreePre: null,
+    degreePost: 'Mgr.',
+    abbreviation: 'KV',
+    isTeacher: false,
+    isActive: true
+  },
+  {
+    username: 'jtoman',
+    name: 'Jiří',
+    surname: 'Toman',
+    degreePre: 'Ing.',
+    degreePost: 'Ph.D.',
+    abbreviation: 'JT',
+    isTeacher: true,
+    isActive: false
+  },
+  {
+    username: 'spospisil',
+    name: 'Světlana',
+    surname: 'Pospíšil',
+    degreePre: 'Bc.',
+    degreePost: null,
+    abbreviation: 'SP',
+    isTeacher: false,
+    isActive: true
+  },
+  {
+    username: 'vhorak',
+    name: 'Václav',
+    surname: 'Horák',
+    degreePre: 'Ing.',
+    degreePost: null,
+    abbreviation: 'VH',
+    isTeacher: true,
+    isActive: false
+  },
+  {
+    username: 'mkubik',
+    name: 'Milan',
+    surname: 'Kubík',
+    degreePre: 'Bc.',
+    degreePost: 'Mgr.',
+    abbreviation: 'MK',
+    isTeacher: false,
+    isActive: true
+  },
+  {
+    username: 'ssvoboda',
+    name: 'Stanislav',
+    surname: 'Svoboda',
+    degreePre: 'Ing.',
+    degreePost: 'Ph.D.',
+    abbreviation: 'SS',
+    isTeacher: true,
+    isActive: false
+  },
+  {
+    username: 'rjurik',
+    name: 'Radek',
+    surname: 'Jurík',
+    degreePre: 'Bc.',
+    degreePost: null,
+    abbreviation: 'RJ',
+    isTeacher: false,
+    isActive: true
+  },
+  {
+    username: 'kchalupova',
+    name: 'Kamila',
+    surname: 'Chalupová',
+    degreePre: 'Mgr.',
+    degreePost: 'Ph.D.',
+    abbreviation: 'KC',
+    isTeacher: true,
+    isActive: false
+  },
+  {
+    username: 'mmaly',
+    name: 'Michal',
+    surname: 'Malý',
+    degreePre: 'Ing.',
+    degreePost: 'Ph.D.',
+    abbreviation: 'MM',
+    isTeacher: false,
+    isActive: true
+  },
+  {
+    username: 'zkolar',
+    name: 'Zdeněk',
+    surname: 'Kolář',
+    degreePre: 'Bc.',
+    degreePost: 'Ph.D.',
+    abbreviation: 'ZK',
+    isTeacher: true,
+    isActive: false
+  },
+  {
+    username: 'ejelinek',
+    name: 'Eva',
+    surname: 'Jelínek',
+    degreePre: 'Mgr.',
+    degreePost: null,
+    abbreviation: 'EJ',
+    isTeacher: false,
+    isActive: true
+  },
+  {
+    username: 'vnespor',
+    name: 'Vít',
+    surname: 'Něspoř',
+    degreePre: 'Bc.',
+    degreePost: 'Ph.D.',
+    abbreviation: 'VN',
+    isTeacher: true,
+    isActive: false
+  },
+  {
+    username: 'mhlousek',
+    name: 'Matěj',
+    surname: 'Hloušek',
+    degreePre: 'Ing.',
+    degreePost: 'Ph.D.',
+    abbreviation: 'MH',
+    isTeacher: false,
+    isActive: true
+  },
+  {
+    username: 'imichalova',
+    name: 'Ivana',
+    surname: 'Michalová',
+    degreePre: 'Bc.',
+    degreePost: null,
+    abbreviation: 'IM',
+    isTeacher: true,
+    isActive: false
+  },
+  {
+    username: 'akratka',
+    name: 'Alžběta',
+    surname: 'Krátká',
+    degreePre: 'Mgr.',
+    degreePost: 'Ph.D.',
+    abbreviation: 'AK',
+    isTeacher: false,
+    isActive: true
+  },
+  {
+    username: 'vlukes',
+    name: 'Vojtěch',
+    surname: 'Lukeš',
+    degreePre: 'Ing.',
+    degreePost: null,
+    abbreviation: 'VL',
+    isTeacher: true,
+    isActive: false
+  },
+  {
+    username: 'lprokop',
+    name: 'Ladislav',
+    surname: 'Prokop',
+    degreePre: 'Bc.',
+    degreePost: 'Ph.D.',
+    abbreviation: 'LP',
+    isTeacher: false,
+    isActive: true
+  },
+  {
+    username: 'mdolezal',
+    name: 'Marek',
+    surname: 'Doležal',
+    degreePre: 'Bc.',
+    degreePost: null,
+    abbreviation: 'MD',
+    isTeacher: true,
+    isActive: false
+  },
+  {
+    username: 'rsvoboda',
+    name: 'Renáta',
+    surname: 'Svoboda',
+    degreePre: 'Ing.',
+    degreePost: null,
+    abbreviation: 'RS',
+    isTeacher: false,
+    isActive: true
+  },
+  {
+    username: 'pvalenta',
+    name: 'Pavlína',
+    surname: 'Valenta',
+    degreePre: 'Bc.',
+    degreePost: 'Ph.D.',
+    abbreviation: 'PV',
+    isTeacher: true,
+    isActive: false
+  },
+  {
+    username: 'dfousek',
+    name: 'David',
+    surname: 'Fousek',
+    degreePre: 'Ing.',
+    degreePost: 'Ph.D.',
+    abbreviation: 'DF',
+    isTeacher: true,
+    isActive: false
+  },
+  {
+    username: 'mjanousek',
+    name: 'Monika',
+    surname: 'Janoušek',
+    degreePre: 'Bc.',
+    degreePost: null,
+    abbreviation: 'MJ',
+    isTeacher: false,
+    isActive: true
+  },
+  {
+    username: 'tturek',
+    name: 'Tomáš',
+    surname: 'Turek',
+    degreePre: 'Bc.',
+    degreePost: null,
+    abbreviation: 'TT',
+    isTeacher: false,
+    isActive: true
+  },
+  {
+    username: 'ihasek',
+    name: 'Iveta',
+    surname: 'Hásek',
+    degreePre: 'Ing.',
+    degreePost: 'Ph.D.',
+    abbreviation: 'IH',
+    isTeacher: true,
+    isActive: false
+  },
+  {
+    username: 'zvlcek',
+    name: 'Zdeněk',
+    surname: 'Vlček',
+    degreePre: 'Mgr.',
+    degreePost: 'Ph.D.',
+    abbreviation: 'ZV',
+    isTeacher: true,
+    isActive: false
+  },
+  {
+    username: 'fstrnad',
+    name: 'František',
+    surname: 'Strnad',
+    degreePre: 'Bc.',
+    degreePost: null,
+    abbreviation: 'FS',
+    isTeacher: false,
+    isActive: true
+  },
+  {
+    username: 'mvondrak',
+    name: 'Michal',
+    surname: 'Vondrák',
+    degreePre: 'Ing.',
+    degreePost: 'Ph.D.',
+    abbreviation: 'MV',
+    isTeacher: true,
+    isActive: false
+  },
+  {
+    username: 'kpokorny',
+    name: 'Kamil',
+    surname: 'Pokorný',
+    degreePre: 'Bc.',
+    degreePost: null,
+    abbreviation: 'KP',
+    isTeacher: false,
+    isActive: true
+  },
+  {
+    username: 'snovotny',
+    name: 'Simona',
+    surname: 'Novotný',
+    degreePre: 'Mgr.',
+    degreePost: 'Ph.D.',
+    abbreviation: 'SN',
+    isTeacher: true,
+    isActive: false
+  },
+  {
+    username: 'pprokop',
+    name: 'Pavel',
+    surname: 'Prokop',
+    degreePre: 'Bc.',
+    degreePost: null,
+    abbreviation: 'PP',
+    isTeacher: true,
+    isActive: false
+  },
+  {
+    username: 'dliska',
+    name: 'Denisa',
+    surname: 'Liška',
+    degreePre: 'Mgr.',
+    degreePost: null,
+    abbreviation: 'DL',
+    isTeacher: false,
+    isActive: true
+  },
+  {
+    username: 'zsvoboda',
+    name: 'Zdeněk',
+    surname: 'Svoboda',
+    degreePre: 'Bc.',
+    degreePost: 'Ph.D.',
+    abbreviation: 'ZS',
+    isTeacher: true,
+    isActive: false
+  },
+  {
+    username: 'lnemec',
+    name: 'Lucie',
+    surname: 'Němec',
+    degreePre: 'Ing.',
+    degreePost: 'Ph.D.',
+    abbreviation: 'LN',
+    isTeacher: false,
+    isActive: true
+  },
+  {
+    username: 'lvybiral',
+    name: 'Ladislav',
+    surname: 'Výbíral',
+    degreePre: 'Bc.',
+    degreePost: null,
+    abbreviation: 'LV',
+    isTeacher: true,
+    isActive: false
+  },
+  {
+    username: 'tkriz',
+    name: 'Tereza',
+    surname: 'Kříž',
+    degreePre: 'Mgr.',
+    degreePost: 'Ph.D.',
+    abbreviation: 'TK',
+    isTeacher: false,
+    isActive: true
+  },
+  {
+    username: 'ksvoboda',
+    name: 'Karel',
+    surname: 'Svoboda',
+    degreePre: 'Ing.',
+    degreePost: 'Ph.D.',
+    abbreviation: 'KS',
+    isTeacher: true,
+    isActive: false
+  },
+  {
+    username: 'jpokorny',
+    name: 'Jana',
+    surname: 'Pokorný',
+    degreePre: 'Bc.',
+    degreePost: 'Ph.D.',
+    abbreviation: 'JP',
+    isTeacher: true,
+    isActive: false
+  },
+  {
+    username: 'tdvorak',
+    name: 'Tomáš',
+    surname: 'Dvořák',
+    degreePre: 'Ing.',
+    degreePost: null,
+    abbreviation: 'TD',
+    isTeacher: false,
+    isActive: true
+  },
+  {
+    username: 'rnovotny',
+    name: 'Radek',
+    surname: 'Novotný',
+    degreePre: 'Bc.',
+    degreePost: 'Ph.D.',
+    abbreviation: 'RN',
+    isTeacher: true,
+    isActive: false
+  },
+  {
+    username: 'ikrejci',
+    name: 'Ivana',
+    surname: 'Krejčí',
+    degreePre: 'Mgr.',
+    degreePost: null,
+    abbreviation: 'IK',
+    isTeacher: false,
+    isActive: true
+  },
+  {
+    username: 'lmartin',
+    name: 'Lucie',
+    surname: 'Martin',
+    degreePre: 'Ing.',
+    degreePost: 'Ph.D.',
+    abbreviation: 'LM',
+    isTeacher: true,
+    isActive: false
+  },
+  {
+    username: 'hliska',
+    name: 'Hana',
+    surname: 'Liška',
+    degreePre: 'Mgr.',
+    degreePost: 'Ph.D.',
+    abbreviation: 'HL',
+    isTeacher: true,
+    isActive: false
+  },
+  {
+    username: 'rkovar',
+    name: 'Radka',
+    surname: 'Kovář',
+    degreePre: 'Bc.',
+    degreePost: null,
+    abbreviation: 'RK',
+    isTeacher: true,
+    isActive: false
+  },
+  {
+    username: 'jholas',
+    name: 'Josef',
+    surname: 'Holás',
+    degreePre: 'Bc.',
+    degreePost: 'Ph.D.',
+    abbreviation: 'JH',
+    isTeacher: true,
+    isActive: false
+  },
+  {
+    username: 'djurik',
+    name: 'Dana',
+    surname: 'Jurík',
+    degreePre: 'Ing.',
+    degreePost: null,
+    abbreviation: 'DJ',
+    isTeacher: false,
+    isActive: true
+  },
+  {
+    username: 'lsramek',
+    name: 'Lukáš',
+    surname: 'Srámeček',
+    degreePre: 'Bc.',
+    degreePost: 'Ph.D.',
+    abbreviation: 'LS',
+    isTeacher: true,
+    isActive: false
+  },
+  {
+    username: 'zprokop',
+    name: 'Zuzana',
+    surname: 'Prokop',
+    degreePre: 'Mgr.',
+    degreePost: null,
+    abbreviation: 'ZP',
+    isTeacher: false,
+    isActive: true
+  },
+  {
+    username: 'pmarek',
+    name: 'Pavel',
+    surname: 'Marek',
+    degreePre: 'Ing.',
+    degreePost: 'Ph.D.',
+    abbreviation: 'PM',
+    isTeacher: true,
+    isActive: false
+  },
+  {
+    username: 'mchalupova',
+    name: 'Martin',
+    surname: 'Chalupová',
+    degreePre: 'Bc.',
+    degreePost: null,
+    abbreviation: 'MC',
+    isTeacher: false,
+    isActive: true
+  },
+  {
+    username: 'sliska',
+    name: 'Simona',
+    surname: 'Liška',
+    degreePre: 'Mgr.',
+    degreePost: 'Ph.D.',
+    abbreviation: 'SL',
+    isTeacher: true,
+    isActive: false
+  },
+  {
+    username: 'vtichy',
+    name: 'Viktor',
+    surname: 'Tichý',
+    degreePre: 'Ing.',
+    degreePost: null,
+    abbreviation: 'VT',
+    isTeacher: false,
+    isActive: true
+  },
+  {
+    username: 'jkral',
+    name: 'Jana',
+    surname: 'Král',
+    degreePre: 'Mgr.',
+    degreePost: null,
+    abbreviation: 'JK',
+    isTeacher: false,
+    isActive: true
+  },
+  {
+    username: 'dmaly',
+    name: 'David',
+    surname: 'Malý',
+    degreePre: 'Ing.',
+    degreePost: 'Ph.D.',
+    abbreviation: 'DM',
+    isTeacher: true,
+    isActive: false
+  }
+];
