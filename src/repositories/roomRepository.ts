d4be4c18e8467bdf86f6451cbcadee8500978315
Daniel Adámek <daniel.adamek123@icloud.com>
2024-07-29 12:24:50 +0200

    room CRUD

---
+++ src/repositories/roomRepository.ts
@@ -0,0 +1,64 @@
+import { Room } from '../models/Room';
+import { RoomType } from '../models/types/RoomType';
+
+/**
+ * RoomDTO interface
+ */
+export interface RoomDTO {
+  name: string;
+  type: RoomType;
+  floor: number;
+  studentCapacity?: number;
+  administratorId: number;
+}
+
+/**
+ * Create a new room
+ * @param data
+ */
+export const createRoom = async (data: RoomDTO): Promise<Room> => {
+  return await Room.create(data as Room);
+};
+
+/**
+ * Get a room by ID
+ * @param id
+ */
+export const getRoomById = async (id: number): Promise<Room | null> => {
+  return await Room.findByPk(id);
+};
+
+/**
+ * Get all rooms
+ */
+export const getAllRooms = async (
+  limit: number,
+  offset: number
+): Promise<{ rows: Room[]; count: number }> => {
+  const { rows, count } = await Room.findAndCountAll({
+    limit,
+    offset
+  });
+
+  return { rows, count };
+};
+
+/**
+ * Update a room
+ * @param id
+ * @param data
+ */
+export const updateRoom = async (
+  id: number,
+  data: Partial<Room>
+): Promise<[affectedCount: number]> => {
+  return await Room.update(data, { where: { roomId: id } });
+};
+
+/**
+ * Delete a room
+ * @param id
+ */
+export const deleteRoom = async (id: number): Promise<number> => {
+  return await Room.destroy({ where: { roomId: id } });
+};
---
+++ src/services/roomService.ts
@@ -0,0 +1,50 @@
+import { Room } from '../models/Room';
+import * as roomRepository from '../repositories/roomRepository';
+import { RoomDTO } from '../repositories/roomRepository';
+
+/**
+ * Create a new room
+ * @param data Room data
+ */
+export const createRoom = async (data: RoomDTO): Promise<Room> => {
+  return await roomRepository.createRoom(data);
+};
+
+/**
+ * Get all rooms
+ */
+export const getAllRooms = async (
+  page: number,
+  limit: number
+): Promise<{ rows: Room[]; count: number }> => {
+  const offset: number = (page - 1) * limit;
+  return await roomRepository.getAllRooms(limit, offset);
+};
+
+/**
+ * Get room by id
+ * @param id Room ID
+ */
+export const getRoomById = async (id: number): Promise<Room | null> => {
+  return await roomRepository.getRoomById(id);
+};
+
+/**
+ * Update a room
+ * @param roomId
+ * @param data
+ */
+export const updateRoom = async (
+  roomId: number,
+  data: RoomDTO
+): Promise<[affectedCount: number]> => {
+  return await roomRepository.updateRoom(roomId, data);
+};
+
+/**
+ * Delete a room
+ * @param roomId
+ */
+export const deleteRoom = async (roomId: number): Promise<number> => {
+  return await roomRepository.deleteRoom(roomId);
+};
