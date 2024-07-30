39b601607f4a549aa41cfc90bcd63fea1a959004
Daniel Adámek <daniel.adamek123@icloud.com>
2024-07-30 11:40:03 +0200

    data for rooms

---
+++ src/tests/data/roomData.ts
@@ -0,0 +1,179 @@
+import { RoomType } from '../../models/types/RoomType';
+
+export const rooms = [
+  {
+    name: '1',
+    type: RoomType.CLASSROOM,
+    floor: 1,
+    studentCapacity: 30,
+    administratorId: 1
+  },
+  {
+    name: '2',
+    type: RoomType.CLASSROOM,
+    floor: 1,
+    studentCapacity: 30,
+    administratorId: 1
+  },
+  {
+    name: '3',
+    type: RoomType.CLASSROOM,
+    floor: 1,
+    studentCapacity: 30,
+    administratorId: 1
+  },
+  {
+    name: '4',
+    type: RoomType.CLASSROOM,
+    floor: 1,
+    studentCapacity: 30,
+    administratorId: 1
+  },
+  {
+    name: '10',
+    type: RoomType.CLASSROOM,
+    floor: 2,
+    studentCapacity: 30,
+    administratorId: 1
+  },
+  {
+    name: '11',
+    type: RoomType.CLASSROOM,
+    floor: 2,
+    studentCapacity: 30,
+    administratorId: 1
+  },
+  {
+    name: '12',
+    type: RoomType.CLASSROOM,
+    floor: 2,
+    studentCapacity: 30,
+    administratorId: 1
+  },
+  {
+    name: '13',
+    type: RoomType.CLASSROOM,
+    floor: 2,
+    studentCapacity: 30,
+    administratorId: 1
+  },
+  {
+    name: '14',
+    type: RoomType.CLASSROOM,
+    floor: 2,
+    studentCapacity: 30,
+    administratorId: 1
+  },
+  {
+    name: '20',
+    type: RoomType.CLASSROOM,
+    floor: 3,
+    studentCapacity: 30,
+    administratorId: 1
+  },
+  {
+    name: '21',
+    type: RoomType.CLASSROOM,
+    floor: 3,
+    studentCapacity: 30,
+    administratorId: 1
+  },
+  {
+    name: '22',
+    type: RoomType.CLASSROOM,
+    floor: 3,
+    studentCapacity: 30,
+    administratorId: 1
+  },
+  {
+    name: '23',
+    type: RoomType.CLASSROOM,
+    floor: 3,
+    studentCapacity: 30,
+    administratorId: 1
+  },
+  {
+    name: 'ŘE',
+    type: RoomType.CLASSROOM,
+    floor: 2,
+    studentCapacity: 30,
+    administratorId: 1
+  },
+  {
+    name: 'ZŘ1',
+    type: RoomType.CLASSROOM,
+    floor: 2,
+    studentCapacity: 30,
+    administratorId: 1
+  },
+  {
+    name: 'ZŘ2',
+    type: RoomType.CLASSROOM,
+    floor: 2,
+    studentCapacity: 30,
+    administratorId: 1
+  },
+  {
+    name: 'L1',
+    type: RoomType.CLASSROOM,
+    floor: 2,
+    studentCapacity: 30,
+    administratorId: 1
+  },
+  {
+    name: 'L2',
+    type: RoomType.CLASSROOM,
+    floor: 2,
+    studentCapacity: 30,
+    administratorId: 1
+  },
+  {
+    name: 'L3',
+    type: RoomType.CLASSROOM,
+    floor: 2,
+    studentCapacity: 30,
+    administratorId: 1
+  },
+  {
+    name: 'D1',
+    type: RoomType.CLASSROOM,
+    floor: 2,
+    studentCapacity: 30,
+    administratorId: 1
+  },
+  {
+    name: 'D2',
+    type: RoomType.CLASSROOM,
+    floor: 2,
+    studentCapacity: 30,
+    administratorId: 1
+  },
+  {
+    name: 'D3',
+    type: RoomType.CLASSROOM,
+    floor: 2,
+    studentCapacity: 30,
+    administratorId: 1
+  },
+  {
+    name: 'K11',
+    type: RoomType.CLASSROOM,
+    floor: 3,
+    studentCapacity: 30,
+    administratorId: 1
+  },
+  {
+    name: 'K12',
+    type: RoomType.CLASSROOM,
+    floor: 3,
+    studentCapacity: 30,
+    administratorId: 1
+  },
+  {
+    name: 'K13',
+    type: RoomType.CLASSROOM,
+    floor: 3,
+    studentCapacity: 30,
+    administratorId: 1
+  }
+];
