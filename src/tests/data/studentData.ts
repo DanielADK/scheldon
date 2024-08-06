76b6534e523cfe3fab381a3a00ba6b189897d275
Daniel Adámek <daniel.adamek123@icloud.com>
2024-08-07 00:03:10 +0200

    student length validation, subclass unique validation, student testdata

---
+++ src/tests/data/studentData.ts
@@ -0,0 +1,39 @@
+import { StudentDTO } from '@repositories/studentRepository';
+
+export const students: StudentDTO[] = [
+  {
+    name: 'Julius',
+    surname: 'Caesar',
+    username: 'jcaesar'
+  },
+  {
+    name: 'Alexander',
+    surname: 'the Great',
+    username: 'agreat'
+  },
+  {
+    name: 'Genghis',
+    surname: 'Khan',
+    username: 'gkhan'
+  },
+  {
+    name: 'Napoleon',
+    surname: 'Bonaparte',
+    username: 'nbon'
+  },
+  {
+    name: 'Cleopatra',
+    surname: 'VII',
+    username: 'cvii'
+  },
+  {
+    name: 'Victoria',
+    surname: 'of the United Kingdom',
+    username: 'vuk'
+  },
+  {
+    name: 'Catherine',
+    surname: 'the Great',
+    username: 'cgreat'
+  }
+];
--- src/tests/scripts/uploadTestData.ts
+++ src/tests/scripts/uploadTestData.ts
@@ -4,25 +4,33 @@ import { employees } from '../data/employeeData';
 import { rooms } from '../data/roomData';
 import { classes } from '../data/classData';
 import { subClasses } from '../data/subClassData';
+import { students } from '../data/studentData';
 import { SubjectDTO } from '@repositories/subjectRepository';
 import { EmployeeDTO } from '@repositories/employeeRepository';
 import { RoomDTO } from '@repositories/roomRepository';
 import { ClassDTO } from '@repositories/classRepository';
 import { SubClassDTO } from '@repositories/subclassRepository';
+import { StudentDTO } from '@repositories/studentRepository';
 
-type entries = SubjectDTO | EmployeeDTO | RoomDTO | ClassDTO | SubClassDTO;
+type entries =
+  | SubjectDTO
+  | EmployeeDTO
+  | RoomDTO
+  | ClassDTO
+  | SubClassDTO
+  | StudentDTO;
 
 const uploadData = async (url: string, entity: string, data: entries[]) => {
-  try {
-    for (const item of data) {
+  for (const item of data) {
+    try {
       const response = await axios.post(url, item);
       console.log(`Uploaded [${entity}]: ${response.data.name}`);
+    } catch (error: any) {
+      console.error(
+        `Error uploading data [${entity}]:`,
+        error.response ? error.response.data : error.message
+      );
     }
-  } catch (error: any) {
-    console.error(
-      `Error uploading data [${entity}]:`,
-      error.response ? error.response.data : error.message
-    );
   }
 };
 
@@ -32,6 +40,7 @@ const uploadTestData = async () => {
   await uploadData('http://localhost:3000/room', 'Room', rooms);
   await uploadData('http://localhost:3000/classes', 'Class', classes);
   await uploadData('http://localhost:3000/subclasses', 'SubClass', subClasses);
+  await uploadData('http://localhost:3000/students', 'Student', students);
   // Add more calls to uploadData for other data sets
 };
 
