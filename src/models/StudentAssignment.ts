dff319a6a99992b92099b6198fc6616701dcc6e2
Daniel Adámek <daniel.adamek123@icloud.com>
2024-08-09 21:35:46 +0200

    SA validators

--- src/models/StudentAssignment.ts
+++ src/models/StudentAssignment.ts
@@ -11,7 +11,12 @@ import {
 import { Class } from '@models/Class';
 import { SubClass } from '@models/SubClass';
 import { Student } from '@models/Student';
-import { Op } from 'sequelize';
+import {
+  validateClassDates,
+  validateClassExistsWhenSubClass,
+  validateInterval,
+  validateSubClassBelongsToClass
+} from '@validators/studentAssignmentValidators';
 
 @Table({
   timestamps: false,
@@ -20,22 +25,7 @@ import { Op } from 'sequelize';
       unique: true,
       fields: ['studentId', 'classId', 'subClassId']
     }
-  ],
-  validate: {
-    datesAreValid(this: Class) {
-      if (new Date(this.validFrom) > new Date(this.validTo)) {
-        throw new Error('validFrom must be less than validTo');
-      }
-    },
-    connectionValidityIsInnerOrEqualThanClasses(this: StudentAssignment) {
-      if (new Date(this.validFrom) < new Date(this.class.validFrom)) {
-        throw new Error('validFrom must be greater than class validFrom');
-      }
-      if (new Date(this.validTo) > new Date(this.class.validTo)) {
-        throw new Error('validTo must be less than class validTo');
-      }
-    }
-  }
+  ]
 })
 export class StudentAssignment extends Model<StudentAssignment> {
   @ForeignKey(() => Student)
@@ -63,10 +53,10 @@ export class StudentAssignment extends Model<StudentAssignment> {
     type: DataType.INTEGER,
     allowNull: true
   })
-  declare subClassId: number;
+  declare subClassId: number | null;
 
   @BelongsTo(() => SubClass)
-  declare subClass: SubClass;
+  declare subClass: SubClass | null;
 
   // Connection validation range
   @Column({
@@ -86,49 +76,10 @@ export class StudentAssignment extends Model<StudentAssignment> {
   // Continuity validation
   @BeforeCreate
   @BeforeUpdate
-  static async validateContinuity(instance: StudentAssignment) {
-    const previousAssignment = await StudentAssignment.findOne({
-      where: {
-        studentId: instance.studentId,
-        classId: instance.classId,
-        subClassId: instance.subClassId,
-        validTo: {
-          [Op.lt]: instance.validFrom
-        }
-      },
-      order: [['validTo', 'DESC']]
-    });
-
-    const nextAssignment = await StudentAssignment.findOne({
-      where: {
-        studentId: instance.studentId,
-        classId: instance.classId,
-        subClassId: instance.subClassId,
-        validFrom: {
-          [Op.gt]: instance.validTo
-        }
-      },
-      order: [['validFrom', 'ASC']]
-    });
-
-    if (
-      previousAssignment &&
-      new Date(previousAssignment.validTo).getTime() !==
-        new Date(instance.validFrom).getTime() - 1000
-    ) {
-      throw new Error(
-        'The new assignment does not start immediately after the previous assignment'
-      );
-    }
-
-    if (
-      nextAssignment &&
-      new Date(instance.validTo).getTime() !==
-        new Date(nextAssignment.validFrom).getTime() - 1000
-    ) {
-      throw new Error(
-        'The new assignment does not end immediately before the next assignment'
-      );
-    }
+  static async validate(instance: StudentAssignment) {
+    await validateSubClassBelongsToClass(instance);
+    await validateClassDates(instance);
+    await validateInterval(instance);
+    await validateClassExistsWhenSubClass(instance);
   }
 }
