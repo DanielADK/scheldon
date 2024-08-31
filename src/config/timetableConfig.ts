438cdc5adbb5ff7b3a152d27304ddf0a77e0c77d
Daniel Adámek <daniel.adamek123@icloud.com>
2024-08-31 22:16:50 +0200

    time operations

---
+++ src/config/timetableConfig.ts
@@ -0,0 +1,12 @@
+export const CLASS_HOURS = [
+  { hour: 0, start: '7:30', end: '8:15' },
+  { hour: 1, start: '8:25', end: '9:10' },
+  { hour: 2, start: '9:20', end: '10:05' },
+  { hour: 3, start: '10:20', end: '11:05' },
+  { hour: 4, start: '11:15', end: '12:00' },
+  { hour: 5, start: '12:10', end: '12:55' },
+  { hour: 6, start: '13:05', end: '13:50' },
+  { hour: 7, start: '14:00', end: '14:45' },
+  { hour: 8, start: '14:55', end: '15:40' },
+  { hour: 9, start: '15:50', end: '16:35' }
+];
